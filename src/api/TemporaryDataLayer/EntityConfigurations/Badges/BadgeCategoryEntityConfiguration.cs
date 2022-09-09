5f7690c8b3339051c874b9457d894b4e58f21071
jusrus01 <jjustinas.ruslys@gmail.com>
2022-09-09 08:03:07 +0300

    .

--- src/api/TemporaryDataLayer/EntityConfigurations/Badges/BadgeCategoryEntityConfiguration.cs
+++ src/api/TemporaryDataLayer/EntityConfigurations/Badges/BadgeCategoryEntityConfiguration.cs
@@ -1,33 +1,33 @@
-﻿namespace TemporaryDataLayer
-{
-    internal class BadgeCategoryEntityConfiguration : EntityTypeConfiguration<BadgeCategory>
-    {
-        public BadgeCategoryEntityConfiguration()
-        {
-            Map(e => e.Requires("IsDeleted").HasValue(false));
+﻿//namespace TemporaryDataLayer
+//{
+//    internal class BadgeCategoryEntityConfiguration : EntityTypeConfiguration<BadgeCategory>
+//    {
+//        public BadgeCategoryEntityConfiguration()
+//        {
+//            Map(e => e.Requires("IsDeleted").HasValue(false));
 
-            Property(u => u.Title)
-                .IsRequired()
-                .HasMaxLength(50);
+//            Property(u => u.Title)
+//                .IsRequired()
+//                .HasMaxLength(50);
 
-            Property(u => u.Description)
-                .HasMaxLength(4000);
+//            Property(u => u.Description)
+//                .HasMaxLength(4000);
 
-            Property(u => u.ModifiedBy)
-                .HasMaxLength(50);
+//            Property(u => u.ModifiedBy)
+//                .HasMaxLength(50);
 
-            Property(u => u.CreatedBy)
-                .HasMaxLength(50);
+//            Property(u => u.CreatedBy)
+//                .HasMaxLength(50);
 
-            HasMany(x => x.RelationshipsWithKudosTypes)
-                .WithRequired()
-                .HasForeignKey(x => x.BadgeCategoryId)
-                .WillCascadeOnDelete(value: false);
+//            HasMany(x => x.RelationshipsWithKudosTypes)
+//                .WithRequired()
+//                .HasForeignKey(x => x.BadgeCategoryId)
+//                .WillCascadeOnDelete(value: false);
 
-            HasMany(x => x.BadgeTypes)
-                .WithRequired()
-                .HasForeignKey(x => x.BadgeCategoryId)
-                .WillCascadeOnDelete(value: true);
-        }
-    }
-}
+//            HasMany(x => x.BadgeTypes)
+//                .WithRequired()
+//                .HasForeignKey(x => x.BadgeCategoryId)
+//                .WillCascadeOnDelete(value: true);
+//        }
+//    }
+//}
