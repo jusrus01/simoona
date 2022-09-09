694107f0f01c6aa1679fc7dcc51b6b571cdffb03
jusrus01 <jjustinas.ruslys@gmail.com>
2022-09-09 08:05:57 +0300

    Added BaseModel configuration

---
+++ src/api/TemporaryDataLayer/EntityConfigurations/BaseModelEntityConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TemporaryDataLayer.EntityConfigurations
+{
+    public class BaseModelEntityConfig : IEntityTypeConfiguration<BaseModel>
+    {
+        public void Configure(EntityTypeBuilder<BaseModel> builder)
+        {
+            builder.HasKey(model => model.Id);
+        }
+    }
+}
