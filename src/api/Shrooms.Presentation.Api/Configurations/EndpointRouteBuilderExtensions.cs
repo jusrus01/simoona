d659bd3d55bb09bca8625b9770703d01a559b231
jusrus01 <jjustinas.ruslys@gmail.com>
2022-11-30 13:53:12 +0200

    Removed comments

--- src/api/Shrooms.Presentation.Api/Configurations/EndpointRouteBuilderExtensions.cs
+++ src/api/Shrooms.Presentation.Api/Configurations/EndpointRouteBuilderExtensions.cs
@@ -8,29 +8,16 @@ namespace Shrooms.Presentation.Api.Configurations
     {
         public static void MapControllerRoutes(this IEndpointRouteBuilder endpoints)
         {
-            // config.Routes.IgnoreRoute("elmah", "{resource}.axd/{*pathInfo}");
-
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
             endpoints.Map("elmah/{resource}.axd/{*pathInfo}", async context =>
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
                 context.Response.StatusCode = StatusCodes.Status404NotFound);
-
-            // This will not be needed since we will handle requests with /storage in the middleware
-            // config.Routes.IgnoreRoute("storage", "storage/{*pathInfo}"); 
-
-//            config.Routes.MapHttpRoute(
-//                name: "DefaultApi",
-//                routeTemplate: "{controller}/{action}/{id}",
-//                defaults: new { controller = "Default", action = "Index", id = RouteParameter.Optional });
+            
             endpoints.MapControllerRoute(
                 name: "DefaultApi",
-                pattern: "{controller}/{action}/{id?}", // TODO: Configure IIS so we would get api/
+                pattern: "{controller}/{action}/{id?}",
                 defaults: new { controller = "Default", action = "Index" });
 
-//            config.Routes.MapHttpRoute(
-//                name: "Errors",
-//                routeTemplate: "Error/{action}/",
-//                defaults: new { action = "NotFound" });
             endpoints.MapControllerRoute(
                 name: "errors",
                 pattern: "error/{action}/",
